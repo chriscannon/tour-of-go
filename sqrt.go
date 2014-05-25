2fad2580999a5563f489245cc7e03f22bebdd2f3
Chris Cannon <ccannon@50onred.com>
2014-05-25 18:42:21 -0400

    Made the code a little cleaner.

--- sqrt.go
+++ sqrt.go
@@ -5,26 +5,26 @@ import (
 	"math"
 )
 
+const delta = 1e-15
+
 func Sqrt(x float64) (float64, int) {
-	z := 1.0
+	z := x
 	p := 0.0
-	iteration := 0
-	for {
-		z = z - (z*z-x)/(2*z)
-		iteration++
-		if math.Abs(p-z) < 1e-15 {
+	iteration := 1
+	for ; ; iteration++ {
+		p, z = z, z-(z*z-x)/(2*z)
+		if math.Abs(p-z) < delta {
 			break
 		}
-		p = z
 	}
 	return z, iteration
 }
 
 func main() {
-	val := 10.0
+	const val = 10.0
 	z, iteration := Sqrt(val)
 	z_math := math.Sqrt(val)
 	fmt.Printf("%d iterations square root: %v\n", iteration, z)
-	fmt.Printf("math package square root: %v\n", math.Sqrt(val))
+	fmt.Printf("math package square root: %v\n", z_math)
 	fmt.Printf("diff: %v\n", math.Abs(z-z_math))
 }
