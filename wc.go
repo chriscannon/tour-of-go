dad5be3fe133babe0c14eb387b2be055ab8516bd
Chris Cannon <ccannon@50onred.com>
2014-05-26 10:38:55 -0400

    Simplified map exercise.

--- wc.go
+++ wc.go
@@ -8,18 +8,9 @@ import (
 
 func WordCount(s string) map[string]int {
 	wordMap := make(map[string]int)
-	words := strings.Fields(s)
-	if len(words) > 0 {
-		for _, word := range words {
-			val, present := wordMap[word]
-			if present {
-				wordMap[word] = val + 1
-			} else {
-				wordMap[word] = 1
-			}
-		}
+	for _, word := range strings.Fields(s) {
+		wordMap[word]++
 	}
-
 	return wordMap
 }
 
