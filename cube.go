649e954c72846f0db901c49e5551dbd3c3802f95
Chris Cannon <ccannon@50onred.com>
2014-05-26 21:16:48 -0400

    Add newton's cube root exercise.

---
+++ cube.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+const delta = 1e-15
+
+func Cbrt(x complex128) complex128 {
+	switch x {
+	case 0:
+		fallthrough
+	case 1:
+		return x
+	default:
+		z := x
+		p := complex128(0)
+		for {
+			p, z = z, z-(cmplx.Pow(z, 3)-x)/(3*cmplx.Pow(z, 2))
+			if cmplx.Abs(p-z) < delta {
+				break
+			}
+		}
+		return z
+	}
+}
+
+func main() {
+	fmt.Println(Cbrt(2))
+}
