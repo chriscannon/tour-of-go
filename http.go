00204c0bea33d580167108e680ab6fbcb9725d3a
Chris Cannon <ccannon@50onred.com>
2014-05-28 08:02:55 -0400

    Completed the http exercise.

--- http.go
+++ http.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+// String is a type that implements a http handler interface for a string.
 type String string
 
 func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, s)
 }
 
+// Struct is a type that implements a http handler interface for a struct of Greeting (string),
+// Punct (string), and Who (string).
 type Struct struct {
 	Greeting string
 	Punct    string
 	Who      string
 }
 
+func (s Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, s)
+}
+
 func main() {
-	// your http.Handle calls here
+	http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
 	http.ListenAndServe("localhost:4000", nil)
 }
