c900d02456f185acedc2d896f6e641ba1c61b34b
Chris Cannon <ccannon@50onred.com>
2014-06-09 08:36:05 -0400

    Initial pass at tree exercise.

---
+++ trees.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"code.google.com/p/go-tour/tree"
+)
+
+func traverse(t *tree, ch chan int) {
+	if t.Left != nil {
+		traverse(t, ch)
+	}
+	fmt.Println(t.Value)
+	if t.Right != nil {
+		traverse(t, ch)
+	}
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	traverse(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool
+
+func main() {
+	ch := make(chan int)
+	Walk(tree.New(1), ch)
+}
